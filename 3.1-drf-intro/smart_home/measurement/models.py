dd950e26ebff459fd2d1931e297c740df28eebea
LucasSteinach <steinach@mail.ru>
2022-10-18 17:59:39 +0400

    5th project. comit #1

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    temperature = models.FloatField()
+    created_at = models.DateField(auto_now_add=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return f'Sensor_{self.sensor}_{self.created_at}'
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,33 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from django.db.models import Max
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.response import Response
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer
+
+
+class SensorListView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    # creates new sensor
+    def post(self, request):
+        sensor = Sensor()
+        sensor.name = request.data.get('name')
+        sensor.description = request.data.get('description')
+        sensor.save()
+        return Response({
+            'message': f'sensor "{sensor.name}" created'
+        })
+
+
+class SensorView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class MeasurementView(CreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = MeasurementSerializer
