1f1c8d10635e3a7b9636af39827bfde2692f92a3
LucasSteinach <steinach@mail.ru>
2022-10-23 02:47:24 +0400

    6th project. commit #1

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,43 +1,71 @@
 from rest_framework import serializers
 
+from .models import Product, StockProduct, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
     # настройте сериализатор для склада
 
     def create(self, validated_data):
+        # print(validated_data)
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
         # создаем склад по его параметрам
+        # print('----\n', positions)
         stock = super().create(validated_data)
 
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            values = {
+                'product': position['product'],
+                'quantity': position['quantity'],
+                'price': position['price'],
+                'stock': stock
+            }
+            StockProduct.objects.create(**values)
 
         return stock
 
     def update(self, instance, validated_data):
+        print(validated_data)
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
+        print(stock)
 
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            values = {
+                'product': position['product'],
+                'quantity': position['quantity'],
+                'price': position['price'],
+                'stock': stock
+            }
+            StockProduct.objects.update_or_create(**values)
 
         return stock
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
 
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_fields = ['id', 'stocks']
+    search_fields = ['title', ]
     # при необходимости добавьте параметры фильтрации
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    filterset_fields = ['id', 'positions__product']
+    search_fields = ['address', ]
     # при необходимости добавьте параметры фильтрации
